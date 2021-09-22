fa416145d9e20686e37371c26a286f641169817d
wang-zhuoran <wangzhuoran200105@126.com>
2021-09-22 19:24:59 +0800

    完成第一问

--- LAB2/EE308_LAB2.cpp
+++ LAB2/EE308_LAB2.cpp
@@ -1,21 +1,53 @@
 #include <iostream>
 #include <fstream>
 #include <stack>
+#include <vector>
+#include <set>
+#include <algorithm>
 using namespace std;
+
+const set<string> keywords = {"auto", "break", "case", "char", "const",
+							  "continue", "default", "do", "double", "else",
+							  "enum", "extern", "float", "for", "goto", "if",
+							  "int", "long", "register", "return", "short",
+							  "signed", "sizeof", "static", "struct", "switch",
+							  "typedef", "union", "unsigned", "void",
+							  "volatile", "while"};
+
+bool isNotLetter(char x)
+{
+	return !((x >= 65) && (x <= 90) || (x >= 97) && (x <= 122));
+}
 
 /* 
 Return the total num of keywords in a code file
 the path directed to.
 */
-int countKeywords(string &path)
+int countKeywords(vector<string> &code)
 {
-	return 1;
+	int total_num = 0;
+	for (int i = 0; i < code.size(); i++)
+	{
+		if (keywords.find(code[i]) != keywords.end())
+		{
+			total_num++;
+		}
+	}
+	return total_num;
+}
+
+/*
+Print the number of "switch case" structures, and output 
+the number of "case" corresponding to each group
+*/
+void countSwitchAndCase(vector<string> &code)
+{
 }
 
 void read(string &path, int &level)
 {
-	cout << "Please type in the path if code file: ";
-	cin >> path;
+	// cout << "Please type in the path if code file: ";
+	// cin >> path;
 	cout << "Please select the level of completion: ";
 	cin >> level;
 	if (level <= 0 || level > 4)
@@ -24,24 +56,43 @@ void read(string &path, int &level)
 	}
 }
 
-void readFile(string &path, string code)
+void readFile(string &path, vector<string> &code)
 {
-	fstream in_file;
-	in_file.open(path);
-	in_file >> code;
+	ifstream file(path);
+	string temp;
+	while (file >> temp)
+	{
+		code.push_back(temp);
+	}
+	// for (int i = 0; i < code.size(); i++)
+	// {
+	// 	cout << code[i] << endl;
+	// }
 	//cout << code << endl;
 }
 
-void solve(string &path, int &level)
+void preprocess(vector<string> &code)
+{
+	for (int i = 0; i < code.size(); i++)
+	{
+		string temp = code[i];
+		temp.erase(std::remove_if(temp.begin(), temp.end(), isNotLetter),
+				   temp.end());
+		code[i] = temp;
+	}
+}
+
+void solve(vector<string> &code, int &level)
 {
 	switch (level)
 	{
 	case 1:
 		cout << "Case 1: Count keywords" << endl;
-		cout << "total num: " << countKeywords(path) << endl;
+		cout << "total num: " << countKeywords(code) << endl;
 		break;
 	case 2:
 		cout << "Case 2" << endl;
+		countSwitchAndCase(code);
 		break;
 	case 3:
 		cout << "Case 3" << endl;
@@ -56,14 +107,17 @@ void solve(string &path, int &level)
 
 int main()
 {
-	string path;
-	string code;
+	string path = "D:\\Study\\third_year\\software_engineering\\local_repo\\software-engineering-labs\\LAB2\\C_test_file.txt";
+	vector<string> code;
 	int level;
 	read(path, level);
 	readFile(path, code);
-	cout << code << endl;
-	//cout << path << " " << level << endl;
-	solve(path, level);
+	preprocess(code);
+	// for (int i = 0; i < code.size(); i++)
+	// {
+	// 	cout << code[i] << endl;
+	// }
+	solve(code, level);
 	system("pause");
 	return 0;
 }
