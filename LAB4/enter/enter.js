7908e30d02b55f064d2e164529a23e028a75583f
SUJIAYUAN <81690108+SU-JIAYUAN@users.noreply.github.com>
2021-10-09 22:11:02 +0800

    enter page implementation update

---
+++ LAB4/enter/enter.js
@@ -0,0 +1,39 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+    
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  toSingleGame(){
+    wx.navigateTo({
+      url: '/pages/singleGame/singleGame',
+    })
+  },
+  toCreateRoom(){
+    wx.navigateTo({
+      url: '/pages/addRoom/addRoom',
+    })
+  }
+  
+})
