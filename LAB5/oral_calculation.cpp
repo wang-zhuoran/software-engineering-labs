550f6ffbe84167175f49001cdacc50cffeccdd41
ZhuoranWang <41529680+wang-zhuoran@users.noreply.github.com>
2021-10-18 17:05:51 +0800

    微调。。

--- LAB5/oral_calculation.cpp
+++ LAB5/oral_calculation.cpp
@@ -13,53 +13,6 @@ int extractGrade(string &grade)
     return num;
 }
 
-int generateEquation(int grade)
-{
-    int ans;
-    if(grade == 1 || grade == 2)
-    {
-
-    }else if(grade == 3 || grade == 4)
-    {
-
-    }else
-    {
-
-    }
-    return ans;
-}
-
-
-
-void read(string &grade, int &num_questions)
-{
-
-}
-
-void solve(string &garde, int &num_questions)
-{
-    int grade_num = extractGrade(grade);
-    int num_correct = 0;
-    int ans;
-    for (int i = 0; i < num_questions; i++)
-    {
-        ans = generateEquation(grade_num);
-        int input;
-        cin >> input;
-        if (ans == input)
-        {
-            num_correct++;
-        }
-    }
-    if (num_correct == num_questions)
-    {
-        cout << "end! All right, that's great! Your score is 100." << endl;
-    }
-    else
-    {
-        cout << "end! Wrong question, your score is " << (num_correct * 0.1 / num_question) * 100 << endl;
-    }
-}
 
 double generateEquation(int grade)
 {
@@ -135,6 +88,37 @@ double generateEquation(int grade)
     }
     return ans;
 }
+
+void read(string &grade, int &num_questions)
+{
+
+}
+
+void solve(string &garde, int &num_questions)
+{
+    int grade_num = extractGrade(grade);
+    int num_correct = 0;
+    int ans;
+    for (int i = 0; i < num_questions; i++)
+    {
+        ans = generateEquation(grade_num);
+        int input;
+        cin >> input;
+        if (ans == input)
+        {
+            num_correct++;
+        }
+    }
+    if (num_correct == num_questions)
+    {
+        cout << "end! All right, that's great! Your score is 100." << endl;
+    }
+    else
+    {
+        cout << "end! Wrong question, your score is " << (num_correct * 0.1 / num_question) * 100 << endl;
+    }
+}
+
 
 
 int main()
