23f824dc7fad0963513fa846e62a0d129ed62b0c
ZhuoranWang <41529680+wang-zhuoran@users.noreply.github.com>
2021-10-18 16:17:30 +0800

    建立大概框架，分配任务

---
+++ LAB5/oral_calculation.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <set>
+
+const set<string> sign = {"+", "-", "*", "/"};
+
+int extractGrade(string &grade)
+{
+    
+}
+
+int generateEquation(int grade)
+{
+    int ans;
+    if(grade == 1 || grade == 2)
+    {
+
+    }else if(grade == 3 || grade == 4)
+    {
+
+    }else
+    {
+
+    }
+    return ans;
+}
+
+
+
+void read(string &grade, int &num_questions)
+{
+
+}
+
+void solve(string &garde, int &num_questions)
+{
+    int grade = extractGrade(grade);
+    int single_mark = 100 / num_questions;
+    
+}
+
+int main()
+{
+    int num_questions;
+    string grade;
+
+    read(grade, num_questions);
+
+    return 0;
+}
